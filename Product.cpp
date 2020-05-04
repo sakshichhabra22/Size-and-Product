e1652d38c70e3695eb9a09e1536ed4f4f6edfc9c
sakshichhabra22 <61674215+sakshichhabra22@users.noreply.github.com>
2020-05-05 00:57:27 +0530

    Create Product.cpp
    Program to find product of two numbers entered by user.

---
+++ Product.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{ 
+    float x,y;
+    cout << "Enter two Numbers: " << endl;
+    cin >> x >> y;
+    cout << "Product of " << x << " and "<< y <<" is " << x*y;
+    return 0; 
+}
