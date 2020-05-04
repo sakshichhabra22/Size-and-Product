4322364c00c108be181701a863fd2c79916b2cd0
sakshichhabra22 <61674215+sakshichhabra22@users.noreply.github.com>
2020-05-05 00:55:39 +0530

    Create Size.cpp
    Program to find size of various data types

---
+++ Size.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    char a;
+    cout <<"Size of char: "<< sizeof(a)<<" byte"<<endl;
+
+    int x;
+    cout <<"Size of int: "<< sizeof(x)<<" bytes"<<endl;
+    float y;
+    cout <<"Size of float: "<< sizeof(y)<<" bytes"<<endl;
+    double z;
+    cout <<"Size of double: "<< sizeof(z)<<" bytes"<<endl;
+    return 0;
+
+}
